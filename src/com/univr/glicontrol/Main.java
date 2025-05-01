90da0c5118933fe7fa93551de9c590a5fdbd66ac
22Chr <136465607+22Chr@users.noreply.github.com>
2025-05-01 09:12:29 +0200

    removed unused MainAccess class, reviewed VM options

--- src/com/univr/glicontrol/Main.java
+++ src/com/univr/glicontrol/Main.java
@@ -1,6 +1,9 @@
 package com.univr.glicontrol;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,6 +15,9 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         try {
+            Parent root = FXMLLoader.load(getClass().getResource("./pl/uiElements/MainAccess.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
             stage.setTitle("Glicontrol");
             stage.show();
         } catch (Exception e) {
