5b0bd4502aa7399695a5fafea4c72d653789b5dd
22Chr <136465607+22Chr@users.noreply.github.com>
2025-05-01 09:16:12 +0200

    Chiarificata la classe Controller

--- src/com/univr/glicontrol/pl/Controller.java
+++ src/com/univr/glicontrol/pl/Controller.java
@@ -1,5 +1,31 @@
 package com.univr.glicontrol.pl;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class Controller {
+    public void apriLoginMedico(ActionEvent event) throws IOException {
+        cambiaScena(event, "/login-medico.fxml");
+    }
+
+    public void apriLoginPaziente(ActionEvent event) throws IOException {
+        cambiaScena(event, "/login-paziente.fxml");
+    }
+
+    public void apriLoginAdmin(ActionEvent event) throws IOException {
+        cambiaScena(event, "/login-admin.fxml");
+    }
+
+    private void cambiaScena(ActionEvent event, String fxmlPath) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource(fxmlPath));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 }
