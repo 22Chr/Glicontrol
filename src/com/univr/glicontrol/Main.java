eb374de7230629963203fd994f0f0157789ef786
22Chr <136465607+22Chr@users.noreply.github.com>
2025-05-01 10:18:18 +0200

    Parent root in main usa Objects.requireNotNull

--- src/com/univr/glicontrol/Main.java
+++ src/com/univr/glicontrol/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -15,7 +17,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("./pl/uiElements/MainAccess.fxml"));
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("./pl/uiElements/MainAccess.fxml")));
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.setTitle("Glicontrol");
