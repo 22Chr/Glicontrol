879c6537f80246c0078f01ce1cf1ef5812c55c5b
annap <anna.martini2004@gmail.com>
2025-05-06 12:25:11 +0200

    Integrate le modifiche nella UI del portale admin

--- src/com/univr/glicontrol/pl/Controllers/PortaleAdminController.java
+++ src/com/univr/glicontrol/pl/Controllers/PortaleAdminController.java
@@ -1,14 +1,18 @@
 package com.univr.glicontrol.pl.Controllers;
 
+import com.univr.glicontrol.bll.ListaMedici;
+import com.univr.glicontrol.bll.ListaPazienti;
+import com.univr.glicontrol.bll.Medico;
+import com.univr.glicontrol.bll.Paziente;
+import com.univr.glicontrol.pl.Models.GetListaPortaleAdmin;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.Window;
@@ -21,15 +25,31 @@ public class PortaleAdminController {
     private Button logoutB;
     @FXML
     private MenuItem medicoMI, pazienteMI;
+    @FXML
+    private ListView<Paziente> listaPazienti;
+    @FXML
+    private ListView<Medico> listaMedici;
 
     @FXML
     private void initialize() {
-        // Puoi inizializzare dati o logica qui
+        // Popola le liste
+        ListaMedici lm = new ListaMedici();
+        ObservableList<Medico> medici = FXCollections.observableArrayList(
+                lm.getListaMedici()
+        );
+        listaMedici.setItems(medici);
+
+        ListaPazienti lp = new ListaPazienti();
+        ObservableList<Paziente> pazienti = FXCollections.observableArrayList(
+                lp.getListaPazienti()
+        );
+        listaPazienti.setItems(pazienti);
     }
+
 
     //LOGOUT
     @FXML
-    private void logout(ActionEvent event) {
+    public void logout(ActionEvent event) {
         //mostra finestra di conferma
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Conferma Logout");
@@ -59,16 +79,16 @@ public class PortaleAdminController {
 
     //MEDICO MENUITEM
     @FXML
-    private void inserisciNuovoMedico(ActionEvent event) {
+    public void inserisciNuovoMedico(ActionEvent event) {
         apriFinestra("/InserisciNuovoMedico.fxml", "Inserisci medico");
     }
 
     //PAZIENTE MENUITEM
     @FXML
-    private void inserisciNuovoPaziente(ActionEvent event) {
+    public void inserisciNuovoPaziente(ActionEvent event) {
         apriFinestra("/view/InserisciNuovoPaziente.fxml", "Inserisci paziente");
     }
-    //DA CREAREEEEEEEEE
+    //! NECESSARIO CREARE GLI FXML
     private void apriFinestra(String fxmlPath, String titoloFinestra) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
