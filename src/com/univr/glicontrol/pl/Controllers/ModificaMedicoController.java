cbbae60bcdf78feb40c77d147b9be67c03d558c6
22Chr <136465607+22Chr@users.noreply.github.com>
2025-05-07 18:22:52 +0200

    Implementati campi schermata modifica medico in riferimento ai dati del medico passati al costruttore del controller ModificaMedicoController

--- src/com/univr/glicontrol/pl/Controllers/ModificaMedicoController.java
+++ src/com/univr/glicontrol/pl/Controllers/ModificaMedicoController.java
@@ -1,11 +1,30 @@
 package com.univr.glicontrol.pl.Controllers;
 
 import com.univr.glicontrol.bll.Medico;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class ModificaMedicoController {
     private Medico m;
 
+    @FXML
+    private TextField CFMedicoTF;
+
+    @FXML
+    private TextField nomeMedicoTF;
+
+    @FXML
+    private TextField cognomeMedicoTF;
+
+    @FXML
+    private TextField emailMedicoTF;
+
+
     public void setMedico(Medico medico) {
        this.m = medico;
+       CFMedicoTF.setText(m.getCodiceFiscale());
+       nomeMedicoTF.setText(m.getNome());
+       cognomeMedicoTF.setText(m.getCognome());
+       emailMedicoTF.setText(m.getEmail());
     }
 }
