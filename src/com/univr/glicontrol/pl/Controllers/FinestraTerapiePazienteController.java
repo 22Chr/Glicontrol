b3120221b033ed36257e9797671fea989824d131
annap <anna.martini2004@gmail.com>
2025-05-22 13:34:45 +0200

    revisione delle finestre e creazione di FinestraTerapiePazienteController

---
+++ src/com/univr/glicontrol/pl/Controllers/FinestraTerapiePazienteController.java
@@ -0,0 +1,8 @@
+package com.univr.glicontrol.pl.Controllers;
+
+public class FinestraTerapiePazienteController {
+    //all'inizio mostra pageStorico e pageButton: si si schiaccia su PageStorico si nasconde pageButton e si apre pageTerapia
+    //se si schiacca pageButton si apre la schermata per aggiungere una nuova terapia
+    //inizializzare la list View
+    //inizializzare i campi in pageTerapia
+}
