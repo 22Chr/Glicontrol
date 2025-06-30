3566f4e067d7ea67a057375c03d3338f3c66e28f
22Chr <136465607+22Chr@users.noreply.github.com>
2025-06-30 12:41:23 +0200

    Modificato padding disturbante in PortaleAdmin.fxml

--- src/com/univr/glicontrol/pl/Controllers/FinestraTerapiePazienteController.java
+++ src/com/univr/glicontrol/pl/Controllers/FinestraTerapiePazienteController.java
@@ -7,7 +7,6 @@ import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
